e471dce8d1bdcb5855bdac989a8e0e71618606dd
elialter <63195898+elialter@users.noreply.github.com>
2021-06-21 21:24:06 +0300

    started programming. parsed csv file

--- ID3.py
+++ ID3.py
@@ -1,7 +1,13 @@
+import numpy
+import csv
+import pandas
 
 
-class ID3(object):
+class ID3:
 
     def fit_predict(self, train, test):
-        print("eli")
-        print("gilad")
+
+        with open('train.csv') as csvfile:
+            data = list(csv.reader(csvfile))
+            last = data[len(data) - 1][1]
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+from ID3 import *
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -12,6 +12,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    id3 = ID3()
+    id3.fit_predict('train.csv', 'train.csv')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
